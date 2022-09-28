00780c7ba877cf82a3cc8824ba316b1c3d6cd9ba
Amos Langat <amos.langat@student.moringaschool.com>
2022-09-29 01:14:50 +0300

    complete assignment

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+
+  const [questions, setQuestions] = useState([]) 
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/questions`)
+    .then(response => response.json())
+    .then((questions) => setQuestions(questions))
+  }, []);
+
+  //onDeleteClick function
+  function handleDeleteClick(id) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+      .then((response) => response.json())
+      // console.log("deleted:", id)
+      .then(() => {
+        const updatedQuestions = questions.filter((question) => question.id !== id);
+        setQuestions(updatedQuestions);
+      });
+  }
+
+  const questionList = questions.map((question) => (
+    <QuestionItem
+      key={question.id}
+      question={question}
+      onDeleteClick={handleDeleteClick}
+    />));
+
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questionList}</ul>
     </section>
   );
 }
